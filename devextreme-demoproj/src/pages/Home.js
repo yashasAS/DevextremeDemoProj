f846a6b138876dd1b7024eee9240cf55a95690b2
Yashas <asyashas@gmail.com>
2023-01-10 15:55:41 +0530

    little change done

--- devextreme-demoproj/src/pages/Home.js
+++ devextreme-demoproj/src/pages/Home.js
@@ -3,8 +3,6 @@ import DataGrid from 'devextreme-react/data-grid';
 import data from "../data/data.json"
 
 
-const allowedPageSizes = [5, 10, 'all'];
-
 export function Home() {
     return (
       <div>
